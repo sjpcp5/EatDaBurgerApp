8f7646356880bd71d92780d7154c7a1df45865fe
saphirah.pociluyko <sjpcp5@gmail.com>
2020-05-09 18:41:27 -0500

    configured JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,29 @@
 const mysql = require('mysql')
 const pn = require('../password')
+var connection
 
-const connection = mysql.createConnection({
-  host: 'localhost',
+if (process.env.JAWDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: 'root',
+    // Your username
+    user: 'root',
 
-  // Your password
-  password: pn,
-  database: 'burgerDB'
+    // Your password
+    password: pn,
+    database: 'burgerDB'
+  })
+}
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting:' + err.stack)
+    return
+  }
+  console.log('connected as id' + connection.threadId)
 })
 module.exports = connection
