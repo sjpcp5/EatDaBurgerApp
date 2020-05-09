7a65c7a738d7c5df218a655d9b11e1b36bfb44d7
saphirah.pociluyko <sjpcp5@gmail.com>
2020-05-09 16:31:54 -0500

    finished business logic

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,46 @@
+/* this is our API 
+attach our handlers until the DOM is fully loaded. */
+$(function () {
+  $('.change-devoured').on('click', function (event) {
+    var id = $(this).data('id')
+    var newDevoured = $(this).data('newdevoured')
+
+    var newDevourState = {
+      devoured: newDevoured
+    }
+
+    // Send the PUT request.
+    $.ajax('/api/burgers/' + id, {
+      type: 'PUT',
+      data: newDevourState
+    }).then(function () {
+      console.log('changed devour to', newDevoured)
+      // Reload the page to get the updated list
+      location.reload()
+    })
+  })
+
+  $('.create-form').on('submit', function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault()
+
+    var newBurger = {
+      burgerName: $('#ca')
+        .val()
+        .trim(),
+      devoured: $('[name=devoured]:checked')
+        .val()
+        .trim()
+    }
+
+    // Send the POST request.
+    $.ajax('/api/burgers', {
+      type: 'POST',
+      data: newBurger
+    }).then(function () {
+      console.log('added a new burger')
+      // Reload the page to get the updated list
+      location.reload()
+    })
+  })
+})
