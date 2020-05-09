1045f6fbd3a03ca0d9e71c91777086748047294a
saphirah.pociluyko <sjpcp5@gmail.com>
2020-05-09 15:19:39 -0500

    created password.js and added to .gitignore

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+const connection = require('./connection')
+
+function selectionALL () {
+  return new Promise((resolve, reject) => {
+    let query
+  })
+}
+
+function insertOne () {
+  return new Promise((resolve, reject) => {
+    let query
+  })
+}
+
+function updateOne () {
+  return new Promise((resolve, reject) => {
+    let query
+  })
+}
