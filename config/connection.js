7ba2be3b28df83ee7786d9183cae4fc2aa70e0e9
saphirah.pociluyko <sjpcp5@gmail.com>
2020-05-09 18:50:14 -0500

    reenter configuration

--- config/connection.js
+++ config/connection.js
@@ -2,28 +2,22 @@ const mysql = require('mysql')
 // const pn = require('../password')
 var connection
 
-if (process.env.JAWDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: 'root',
-
-    // Your password
-    password: 'YourPassword',
-    database: 'burgerDB'
+    password: 'password1',
+    database: 'burgers_db'
   })
 }
 connection.connect(function (err) {
   if (err) {
-    console.error('error connecting:' + err.stack)
+    console.error('error connecting: ' + err.stack)
     return
   }
-  console.log('connected as id' + connection.threadId)
+  console.log('connected as id ' + connection.threadId)
 })
 module.exports = connection
